5d950e452d6e5b42cb43ca8503e4cb93f5728df8
Jungminchae <minchae3618@gmail.com>
2023-09-24 22:06:09 +0900

    Add langchain but does not work

--- app/routes/apis/messages.py
+++ app/routes/apis/messages.py
@@ -3,6 +3,7 @@ from fastapi.responses import StreamingResponse
 from app.services.dependencies.messages import (
     HelloMessagePrompt,
     HelloMessageStreamingPrompt,
+    HelloMessageLangchainStreamingPrompt,
 )
 from app.services.dependencies.requests import check_secret_header
 from app.schemas.messages import MessageResponse
@@ -31,13 +32,35 @@ async def get_generated_messages(results: HelloMessagePrompt):
 
 
 @router.post(
-    "/streaming",
+    "/streaming/",
     dependencies=[Depends(check_secret_header)],  # FIXME: 추후 변경
     response_model=str,
     status_code=status.HTTP_200_OK,
-    summary="인사마 생성하기 (스트리밍)",
+    summary="인사말 생성하기 (스트리밍)",
 )
 async def get_generated_messages_streaming(results: HelloMessageStreamingPrompt):
+    """
+    ## OPEN AI에 프롬프트 전달하기
+    - name: str (Optional)
+    - relation: str
+    - reason: str
+    - manner: str
+    - max_length: str
+    """
+    return StreamingResponse(results, media_type="text/event-stream")
+
+
+# FIXME: 실행 안됨.. 원인 파악 필요
+@router.post(
+    "/langchain/streaming/",
+    dependencies=[Depends(check_secret_header)],  # FIXME: 추후 변경
+    response_model=str,
+    status_code=status.HTTP_200_OK,
+    summary="인사말 생성하기 (스트리밍, Langchain)",
+)
+async def get_generated_messages_langchain_streaming(
+    results: HelloMessageLangchainStreamingPrompt,
+):
     """
     ## OPEN AI에 프롬프트 전달하기
     - name: str (Optional)
--- app/services/dependencies/messages.py
+++ app/services/dependencies/messages.py
@@ -1,6 +1,11 @@
+import asyncio
 import openai
-from typing import Annotated
+from typing import Annotated, AsyncIterable
 from fastapi import Depends, HTTPException, status
+from langchain.chat_models import ChatOpenAI
+from langchain.callbacks import AsyncIteratorCallbackHandler
+from langchain.schema import HumanMessage, SystemMessage, AIMessage
+from langchain.schema.exceptions import LangChainException
 from app.core.prompts.messages import MessageBotPrompt
 from app.core.config import settings
 from app.schemas.messages import MessageRequest
@@ -43,7 +48,7 @@ async def get_streaming_message_from_opeanai(
     data: Annotated[MessageRequest, Depends(convert_manner)]
 ) -> dict:
     try:
-        response = await openai.ChatStream.acreate(
+        response = await openai.ChatCompletion.acreate(
             model=settings.MODEL,
             messages=MessageBotPrompt.make_prompt(data),
             temperature=1,
@@ -53,14 +58,62 @@ async def get_streaming_message_from_opeanai(
             presence_penalty=0,
             stream=True,
         )
-        content = response.choices[0]["message"]["content"]
+        async for token in response:
+            try:
+                yield token.choices[0]["message"]["content"]
+            except KeyError:
+                ...
     except openai.InvalidRequestError as e:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
 
-    return content
+    # return content
+
+
+async def get_streaming_message_with_langchain(
+    data: Annotated[MessageRequest, Depends(convert_manner)]
+) -> AsyncIterable[str]:
+    callback = AsyncIteratorCallbackHandler()
+    model = ChatOpenAI(
+        model=settings.MODEL,
+        temperature=1,
+        max_tokens=1024,
+        openai_api_key=settings.OPEN_AI_KEY,
+    )
+    task = asyncio.create_task(
+        model.agenerate(
+            messages=[
+                [
+                    SystemMessage(content=MessageBotPrompt._get_system()["content"]),
+                    HumanMessage(
+                        content=MessageBotPrompt._get_user(
+                            settings.BASE_MESSAGE_PROMPT, data
+                        )["content"]
+                    ),
+                    AIMessage(
+                        content=MessageBotPrompt._get_assistant(
+                            settings.BASE_MESSAGE_PROMPT
+                        )["content"]
+                    ),
+                ]
+            ]
+        )
+    )
+
+    try:
+        async for token in callback.aiter():
+            yield token
+    except LangChainException as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+    finally:
+        callback.done.set()
+
+    await task
 
 
 HelloMessagePrompt = Annotated[dict, Depends(get_message_from_openai)]
 HelloMessageStreamingPrompt = Annotated[
     str, Depends(get_streaming_message_from_opeanai)
+]
+HelloMessageLangchainStreamingPrompt = Annotated[
+    AsyncIterable[str], Depends(get_streaming_message_with_langchain)
 ]
